f2d7e751c54bc6e1ac0a70f7e98331f079f9ef46
Sukhpreetsanghu <sanghusukhpreet@gmail.com>
2026-05-03 18:14:54 +0530

    Create task model with schema in task.js

---
+++ models/task.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new
+  mongoose.Schema({
+    title:String,
+    status:{type:String, default:
+      "pending"},
+    assignedTo: String,
+    dueDate: Date
+  });
+
+module.exports=
+  mongoose.model("Task",taskSchema);
